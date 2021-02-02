8d2e713a496a160a14e880bccf7eab69f55ffc70
Tom Manner <tsmanner@us.ibm.com>
2021-02-02 18:47:30 -0500

    Wrote a parenthetical parser

--- assets/js/dnd/parsers.js
+++ assets/js/dnd/parsers.js
@@ -3,33 +3,47 @@ DnD.Parsers = {}
 
 DnD.Parsers.Parser = class {
   constructor() {}
-
   parse(inString) {
     return {
       remainder: inString
     }
   }
-
 }
 
 
 DnD.Parsers.AttributeReference = class extends DnD.Parsers.Parser {
   constructor() { super(); }
-
-  parse(attributeReferenceString) {
-    let match = attributeReferenceString.match(/^(?<id>[\w\.]*):(?<attribute>[\w\.]*)/);
+  parse(parseString) {
+    let match = parseString.match(/^(?<id>[\w\.]*):(?<attribute>[\w\.]*)/);
     if (match) {
       return {
         id: match.groups.id,
         attribute: match.groups.attribute,
-        ...super.parse(attributeReferenceString.slice(match[0].length))
+        ...super.parse(parseString.slice(match[0].length))
       }
     }
     return {
       id: "",
       attribute: "",
-      ...super.parse(attributeReferenceString)
+      ...super.parse(parseString)
     }
   }
+}
 
+
+DnD.Parsers.Parenthetical = class extends DnD.Parsers.Parser {
+  constructor(innerParser) { super(); this.innerParser = innerParser; }
+  parse(parseString) {
+    let match = parseString.match(/^\(/);
+    if (match) {
+      result = this.innerParser.parse(parseString.slice(match[0].length));
+      if (result.remainder.match(/^\)/)) {
+        return {
+          ...result,
+          remainder: result.remainder
+        }
+      }
+    }
+    return super.parse(parseString);
+  }
 }
