33f9715376cb85b5192aef5da960820aeab9d84b
Tom Manner <tsmanner@us.ibm.com>
2021-02-04 12:55:58 -0500

    Renamed value to valueReference

--- assets/js/dnd/value.js
+++ assets/js/dnd/value.js
@@ -4,29 +4,29 @@ DnD.Parsers.IdAttrs = new DnD.Parsers.SepBy(/ /, DnD.Parsers.IdAttr);
 
 DnD.Value = {
 
-  initValue: function(element) {
-    if ("source" in element.dataset) {
-      let ast = DnD.Parsers.IdAttr.parse(element.dataset.source);
+  initValueReference: function(element) {
+    if ("valueSource" in element.dataset) {
+      let ast = DnD.Parsers.IdAttr.parse(element.dataset.valueSource);
       console.log(ast);
       if (ast.status == "ok") {
-        if (ast.length == element.dataset.source.length) {
+        if (ast.length == element.dataset.valueSource.length) {
           let sourceId = ast.children[0].children[0];
           let sourceAttr = ast.children[2].children[0];
           console.log(sourceId, sourceAttr);
           element.innerHTML = document.getElementById(sourceId).dataset[sourceAttr];
         }
         else {
-          throw "Multiple element.dataset.sources '"+element.dataset.source+"'";
+          throw "Multiple element.dataset.sources '"+element.dataset.valueSource+"'";
         }
       }
       else {
-        throw "Invalid element.dataset.source '"+element.dataset.source+"'";
+        throw "Invalid element.dataset.valueSource '"+element.dataset.valueSource+"'";
       }
     }
   },
 
   initValues: function() {
-    Array.from(document.getElementsByClassName("value")).map(DnD.Value.initValue);
+    Array.from(document.getElementsByClassName("value-reference")).map(DnD.Value.initValueReference);
   },
 
 }
