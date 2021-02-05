10c486c3b9f9043fd2e516b7ab4fad3aefb5cdd0
Tom Manner <tsmanner@us.ibm.com>
2021-02-05 01:05:51 -0500

    Added console log of initActions

--- assets/js/dnd/actions.js
+++ assets/js/dnd/actions.js
@@ -9,7 +9,8 @@ DnD.Actions = {
   },
 
   initAction: function(element) {
-
+    let eventWithKeys = DnD.Parsers.extractEventWithKeys(DnD.Parsers.EventWithKeys.parse(element.dataset.actions));
+    console.log(eventWithKeys);
   },
 
   initActions: function() { Array.from(document.getElementsByClassName("action")).map(DnD.Actions.initAction); },
