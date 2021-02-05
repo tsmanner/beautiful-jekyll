256e4b82cca4efe2fc9dbdafdae074b192ea4c2a
Tom Manner <tsmanner@us.ibm.com>
2021-02-04 21:21:47 -0500

    Consolidated value and value-reference, allowing the data-value field to take the form of a number or an ID:ATTR

--- assets/js/dnd/parsers.js
+++ assets/js/dnd/parsers.js
@@ -207,6 +207,7 @@ DnD.Parsers.ElementId = /[a-zA-Z_][\w\.]*/;
 DnD.Parsers.AttributeName = /[a-zA-Z_]\w*/;
 DnD.Parsers.IdAttr  = new DnD.Parsers.Sequence(new DnD.Parsers.Optional(DnD.Parsers.ElementId), /:/, new DnD.Parsers.Optional(DnD.Parsers.AttributeName));
 DnD.Parsers.IdAttrs = new DnD.Parsers.SepBy(/ /, DnD.Parsers.IdAttr);
+DnD.Parsers.ValueOrIdAttr = new DnD.Parsers.Alternatives(DnD.Parsers.IdAttr, /\d+/);
 
 DnD.extractIdAttr = function(ast) {
   if (ast.status == "ok") {
--- assets/js/dnd/value.js
+++ assets/js/dnd/value.js
@@ -1,17 +1,25 @@
 DnD.Value = {
 
-  initValueReference: function(element) {
-    if ("valueSource" in element.dataset) {
-      let source = DnD.extractIdAttr(DnD.Parsers.IdAttr.parse(element.dataset.valueSource));
-      if (source.id == null) { source.id = element.id; }
-      if (source.attr == null) { source.attr = "value"; }
-      element.innerHTML = document.getElementById(source.id).dataset[source.attr];
+  initValue: function(element) {
+    if ("value" in element.dataset) {
+      let parseResult = DnD.Parsers.ValueOrIdAttr.parse(element.dataset.value);
+      if (parseResult.status == "ok") {
+        if (parseResult.children[0].matcher instanceof RegExp) {
+          element.innerHTML = parseResult.children[0].children[0];
+        }
+        else {
+          let source = DnD.extractIdAttr(parseResult.children[0]);
+          if (source.id == null) { source.id = element.id; }
+          if (source.attr == null) { source.attr = "value"; }
+          element.innerHTML = document.getElementById(source.id).dataset[source.attr];
+        }
+      }
     }
   },
 
-  initValueReferences: function() { Array.from(document.getElementsByClassName("value-reference")).map(DnD.Value.initValueReference); },
+  initValues: function() { Array.from(document.getElementsByClassName("value")).map(DnD.Value.initValue); },
 
 }
 
 
-DnD.initFunctions.push(DnD.Value.initValueReferences);
+DnD.initFunctions.push(DnD.Value.initValues);
