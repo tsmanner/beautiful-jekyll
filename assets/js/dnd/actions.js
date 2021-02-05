790df6a917275ea5403c62e9df8758a3caa6aeec
Tom Manner <tsmanner@us.ibm.com>
2021-02-05 14:55:44 -0500

    Iterate through all eventsWithKeys

--- assets/js/dnd/actions.js
+++ assets/js/dnd/actions.js
@@ -11,8 +11,10 @@ DnD.Actions = {
   initAction: function(element) {
     let eventsWithKeys = DnD.Parsers.extractEventsWithKeys(DnD.Parsers.EventsWithKeys.parse(element.dataset.actions));
     console.log(eventsWithKeys);
-    let keys = eventsWithKeys.keys.reduce(function (keys, key) { keys[key] = true; return keys; }, { alt: false, ctrl: false, shift: false });
-    console.log(keys);
+    for (const eventWithKeys of eventsWithKeys) {
+      let keys = eventWithKeys.keys.reduce(function (keys, key) { keys[key] = true; return keys; }, { alt: false, ctrl: false, shift: false });
+      console.log(keys);
+    }
   },
 
   initActions: function() { Array.from(document.getElementsByClassName("action")).map(DnD.Actions.initAction); },
