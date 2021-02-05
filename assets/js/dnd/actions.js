cde07f084b8443a6de2c129cb3decd074a50d1b6
Tom Manner <tsmanner@us.ibm.com>
2021-02-05 14:49:52 -0500

    Created keyname->true|false mapping

--- assets/js/dnd/actions.js
+++ assets/js/dnd/actions.js
@@ -11,6 +11,8 @@ DnD.Actions = {
   initAction: function(element) {
     let eventsWithKeys = DnD.Parsers.extractEventsWithKeys(DnD.Parsers.EventsWithKeys.parse(element.dataset.actions));
     console.log(eventsWithKeys);
+    let keys = eventsWithKeys.keys.reduce(function (keys, key) { keys[key] = true; return keys; }, { alt: false, ctrl: false, shift: false });
+    console.log(keys);
   },
 
   initActions: function() { Array.from(document.getElementsByClassName("action")).map(DnD.Actions.initAction); },
